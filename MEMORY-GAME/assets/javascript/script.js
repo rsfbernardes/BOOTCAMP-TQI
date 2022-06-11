d16a446750490cfff101c6a9e0c0129fe7e0a03e
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-06-11 12:15:28 -0300

    funtion resetBoard

--- MEMORY-GAME/assets/javascript/script.js
+++ MEMORY-GAME/assets/javascript/script.js
@@ -5,7 +5,8 @@ let lockBoard = false;
 
 function flipCard() {
     if(lockBoard) return;
-    
+    if (this === firstCard) return;
+
     this.classList.add('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
@@ -24,19 +25,30 @@ function checkForMatch() {
         return;
     }
 
-    unflipCard();
+    unflipCards();
 }
 
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
-function unflipCard() {
+function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500)
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] =  [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
