c04f7f39e188f13c2c20bb19490988abdeb2a228
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-06-11 12:05:02 -0300

    funtions unflipCard, disableCards, checkForMatch

--- MEMORY-GAME/assets/javascript/script.js
+++ MEMORY-GAME/assets/javascript/script.js
@@ -1,8 +1,11 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
-let firstCard, SecondCard;
+let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+    if(lockBoard) return;
+    
     this.classList.add('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
@@ -10,8 +13,30 @@ function flipCard() {
         return;
     }
 
-    SecondCard = this;
+    secondCard = this;
+    hasFlippedCard = false;
     checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCard();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCard() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500)
 }
 
 cards.forEach((card) => {
