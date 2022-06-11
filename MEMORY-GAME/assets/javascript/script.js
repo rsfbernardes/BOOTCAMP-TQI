164e64411b2537b37a97fbd13c7727cf7908a147
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-06-11 11:43:51 -0300

    function flipCard

--- MEMORY-GAME/assets/javascript/script.js
+++ MEMORY-GAME/assets/javascript/script.js
@@ -1,9 +1,20 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, SecondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    SecondCard = this;
+    checkForMatch();
 }
 
 cards.forEach((card) => {
-    card.addEventListener('click', flipCard)
+    card.addEventListener('click', flipCard);
 })
+
