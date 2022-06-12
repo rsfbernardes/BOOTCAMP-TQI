edc44b3415fd4de4d844bd0a3452c7a8ca532159
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-06-12 17:57:35 -0300

    Smartcounter

--- HOOKS-CLASS--STATES-EFFECTS/hook-class/src/Components/App/App.js
+++ HOOKS-CLASS--STATES-EFFECTS/hook-class/src/Components/App/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Counter from '../Counter/Counter';
+import SmartCounter from '../Smartcounter/SmartCounter';
 
 function App() {
 
   return (
     <>
       <h1>Hello World</h1>
-      <Counter />
+      <SmartCounter />
     </>
   );
 }
