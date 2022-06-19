b255c305586e8e97dde0cfe6d95b6f0219c17406
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-06-19 15:52:54 -0300

    styled Profile

--- GITHUB-API/github-api/src/components/profile/index.js
+++ GITHUB-API/github-api/src/components/profile/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import * as Styled from './styled'
 
 function Profile() {
-  return (
-    <>
-        <img src="https://avatars.githubusercontent.com/u/61915671?v=4" alt="Avatar of user" />
-        <div>Rodrigo Bernardes</div>
-        <div>Username:</div>
-        <span>rsfbernardes</span>
-        <div>location: </div>
-        <span>Uberlândia</span>
-        <div>Bio: </div>
-        <span>I'm from Brazil, married, father of a girl.\r\nPassionate about sports and programming</span>
-    </>
-  )
+    return (
+        <Styled.WrapperProfile>
+            <Styled.ProfilePhoto>
+                <img src="https://avatars.githubusercontent.com/u/61915671?v=4" alt="Avatar of user" />
+            </Styled.ProfilePhoto>
+            <Styled.ProfileData>
+                <div>Rodrigo Bernardes</div>
+                <div>Username:</div>
+                <span>rsfbernardes</span>
+                <div>location: </div>
+                <span>Uberlândia</span>
+                <div>Bio: </div>
+                <span>I'm from Brazil, married, father of a girl.\r\nPassionate about sports and programming</span>
+            </Styled.ProfileData>
+        </Styled.WrapperProfile>
+    )
 }
 
 export default Profile
---
+++ GITHUB-API/github-api/src/components/profile/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const WrapperProfile = styled.div`
+
+    display: flex;
+    background-color: red;
+`
+
+export const ProfilePhoto = styled.div`
+
+    && img {
+        border-radius: 50%;
+    }
+`
+
+export const ProfileData = styled.div`
+
+    
+`
