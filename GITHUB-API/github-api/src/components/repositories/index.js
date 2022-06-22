4bb37c37ae23b4c4fdc56b107ca9ad8c588af414
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-06-21 23:55:54 -0300

    styled.js, index.js, from repositories

--- GITHUB-API/github-api/src/components/repositories/index.js
+++ GITHUB-API/github-api/src/components/repositories/index.js
@@ -3,9 +3,17 @@ import * as Styled from './styled'
 
 function Repositories() {
   return (
-    <Styled.Container>
-      <div>Repositories</div>
-    </Styled.Container>
+    <Styled.WrapperTabs
+      selectedTabClassName='is-selected'
+      selectedTabPanelClassName='is-selected'
+    >
+      <Styled.WrapperTabList >
+        <Styled.WrapperTab >Repositories</Styled.WrapperTab>
+        <Styled.WrapperTab >Starred</Styled.WrapperTab>
+      </Styled.WrapperTabList>
+      <Styled.WrapperTabPanel>Repositories</Styled.WrapperTabPanel>
+      <Styled.WrapperTabPanel>Starred</Styled.WrapperTabPanel>
+    </Styled.WrapperTabs>
   )
 }
 
--- GITHUB-API/github-api/src/components/repositories/styled.js
+++ GITHUB-API/github-api/src/components/repositories/styled.js
@@ -1,7 +1,40 @@
 import styled from "styled-components";
 import { Tabs, TabList, Tab, TabPanel } from 'react-tabs'
 
-export const Container = styled.div`
+export const WrapperTabs = styled(Tabs)`
 
-    
+    font-size: 24px;
+`
+
+export const WrapperTabList = styled(TabList)`
+
+    display: flex;
+    margin-bottom: 0;
+    padding: 0;
+`
+
+export const WrapperTab = styled(Tab)`
+
+    list-style: none;
+    margin-right: 10px;
+    border: 1px dotted gray;
+    border-radius: 8px;
+    padding: 5px;
+    user-select: none;
+    cursor: pointer;
+
+    &:focus {
+        box-shadow: 0 0 0.4em gray;
+    }
+`
+
+export const WrapperTabPanel = styled(TabPanel)`
+
+    padding-left: 5px;
+    min-height: 40vh;
+    display: none;
+
+    &.is-selected {
+        display: inline-block;
+    }
 `
