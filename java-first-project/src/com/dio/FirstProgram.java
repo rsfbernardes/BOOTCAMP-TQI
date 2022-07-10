dd7cc822c4064be965519df8e4a4cd69ba006ed6
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-07-10 15:58:15 -0300

    first commit java

---
+++ java-first-project/src/com/dio/FirstProgram.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+import com.dio.model.Cat;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+
+		Cat cat = new Cat();
+		System.out.println(cat);
+
+		/*
+		 * int a = 2; int b = 3; System.out.println("Hello World! " + (a+b));
+		 */
+	}
+}
