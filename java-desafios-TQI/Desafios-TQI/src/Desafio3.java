d94b81faf77dd7c6117f693dde53a885e9e56d51
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-07-19 09:32:57 -0300

    java Desafios TQI - refactoring Desafio 3

--- java-desafios-TQI/Desafios-TQI/src/Desafio3.java
+++ java-desafios-TQI/Desafios-TQI/src/Desafio3.java
@@ -4,21 +4,37 @@ public class Desafio3 {
 
     public static void main(String[] args) {
 
-        double distancia = 0d;
+        double distancia, X1, X2, Y1, Y2;
+        String[] linha1;
+        String[] linha2;
 
         Scanner scan = new Scanner(System.in);
 
-        //System.out.println("Entre com a coordenada X1: ");
-        double X1 = scan.nextDouble();
-        //System.out.println("Entre com a coordenada Y1: ");
-        double Y1 = scan.nextDouble();
-        //System.out.println("Entre com a coordenada X2: ");
-        double X2 = scan.nextDouble();
-        //System.out.println("Entre com a coordenada Y2: ");
-        double Y2 = scan.nextDouble();
+        for (int i = 0; i < 2; i++) {
 
-        distancia = Math.sqrt(((X2 - X1) * (X2 - X1)) + ((Y2 - Y1) * (Y2 - Y1)));
+            //quebra string em várias substrings a partir de um caracter
+            linha1 = scan.nextLine().split(" ");
+            linha2 = scan.nextLine().split(" ");
 
-        System.out.printf("%.4f", distancia);
+
+            X1 = Double.parseDouble(linha1[0]);
+            Y1 = Double.parseDouble(linha1[1]);
+            X2 = Double.parseDouble(linha2[0]);
+            Y2 = Double.parseDouble(linha2[1]);
+
+//        //System.out.println("Entre com a coordenada X1: ");
+//        double X1 = scan.nextDouble();
+//        //System.out.println("Entre com a coordenada Y1: ");
+//        double Y1 = scan.nextDouble();
+//        //System.out.println("Entre com a coordenada X2: ");
+//        double X2 = scan.nextDouble();
+//        //System.out.println("Entre com a coordenada Y2: ");
+//        double Y2 = scan.nextDouble();
+
+            distancia = Math.sqrt(((X2 - X1) * (X2 - X1)) + ((Y2 - Y1) * (Y2 - Y1)));
+
+            System.out.printf("%.4f", distancia);
+            return;
+        }
     }
 }
---
+++ java-desafios-TQI/Desafios-TQI/src/Desafio4.java
@@ -0,0 +1,45 @@
+// TODO: complete os espaços em branco com sua solução para o problema
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.util.*;
+
+public class Desafio4{
+
+    public static void main(String[] args) {
+
+        //a classe Scanner auxilia na leitura dos dados de entrada
+        Scanner leitor = new Scanner(System.in);
+
+        int QT = Integer.parseInt(leitor.nextLine());
+
+        String[] linha1;
+        String[] linha2;
+        String nome1;
+        String escolha1;
+        String nome2;
+        String escolha2;
+        int N, M;
+
+        for (int i = 0; i < QT; i++) {
+
+            //quebra string em várias substrings a partir de um caracter
+            linha1 = leitor.nextLine().split(" ");
+            linha2 = leitor.nextLine().split(" ");
+
+
+            N = Integer.parseInt(linha2[0]);
+            M = Integer.parseInt(linha2[1]);
+
+            nome1 = linha1[0];
+            escolha1 = linha1[1];
+
+            nome2 = linha1[2];
+            escolha2 = linha1[3];
+
+
+            if (((N + M) % 2 == 0 && escolha1.equalsIgnoreCase("PAR")) ||
+                    ((N + M) % 2 != 0 && escolha1.equalsIgnoreCase("IMPAR"))) System.out.println(nome1);
+            else if (((N + M) % 2 == 0 && escolha2.equalsIgnoreCase("PAR")) ||
+                    ((N + M) % 2 != 0 && escolha2.equalsIgnoreCase("IMPAR"))) System.out.println(nome2);System.out.println(nome2);
+        }
+    }
+}
